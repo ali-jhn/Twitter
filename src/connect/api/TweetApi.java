c61d3b7367e4d138e7801058626126ae4bea475e
Ali Jahan <alijahan890@gmail.com>
2024-03-01 17:26:13 +0330

    Necessary api added

---
+++ src/connect/api/TweetApi.java
@@ -0,0 +1,85 @@
+package connect.api;
+
+import connect.service.TweetService;
+import model.Response;
+import model.exception.InvalidTypeException;
+import model.exception.UserThreadNotFoundException;
+import model.request.tweet.*;
+
+
+public class TweetApi {
+
+    private final TweetService service;
+    //sender object to send responses to client
+    private final Sender sender;
+
+    protected TweetApi() {
+        service = new TweetService();
+        sender = Sender.getSender();
+    }
+
+
+    protected void getRequest(TweetRequest tweetRequest) throws InvalidTypeException {
+        switch(tweetRequest.getSubType()) {
+            case TWEET:
+                tweet((TweetReq) tweetRequest);
+                break;
+            case RETWEET:
+                retweet((RetweetReq) tweetRequest);
+                break;
+            case QUOTE:
+                quote((QuoteReq) tweetRequest);
+                break;
+            case REPLY:
+                reply((ReplyReq) tweetRequest);
+                break;
+            case LIKE:
+                like((LikeTweetReq) tweetRequest);
+                break;
+            default:
+                throw new InvalidTypeException();
+        }
+
+    }
+
+    private void like(LikeTweetReq tweetRequest){
+        handleResponse(service.like(tweetRequest) , tweetRequest);
+    }
+
+
+    private void tweet(TweetReq tweetRequest)
+    {
+        handleResponse(service.tweet(tweetRequest) , tweetRequest);
+    }
+
+    private void retweet(RetweetReq tweetRequest)
+    {
+        handleResponse(service.retweet(tweetRequest) , tweetRequest);
+    }
+
+    private void quote(QuoteReq tweetRequest)
+    {
+        handleResponse(service.quote(tweetRequest) , tweetRequest);
+    }
+
+    private void reply(ReplyReq tweetRequest)
+    {
+        handleResponse(service.reply(tweetRequest) , tweetRequest);
+    }
+
+
+    private void handleResponse(Response response , TweetRequest request)
+    {
+        sendResponse(response);
+    }
+
+    private void sendResponse(Response response)
+    {
+        try {
+            sender.sendResponse(response);
+        } catch (UserThreadNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/connect/api/UserApi.java
@@ -0,0 +1,161 @@
+package connect.api;
+
+import connect.service.UserService;
+import model.Response;
+import model.exception.InvalidTypeException;
+import model.exception.UserThreadNotFoundException;
+import model.request.user.*;
+
+
+public class UserApi {
+    private final UserService service;
+    private final Sender sender;
+
+    protected UserApi()
+    {
+        service = new UserService();
+        sender = Sender.getSender();
+    }
+
+
+    protected void getRequest(UserRequest request) throws InvalidTypeException {
+        switch(request.getSubType()) {
+            case MY_PROFILE:
+                get_profile((MyProfileReq) request);
+                break;
+            case SET_AVATAR:
+                set_avatar((AvatarReq) request);
+                break;
+            case SET_HEADER:
+                set_header((HeaderReq) request);
+                break;
+            case SET_BIO:
+                set_bio((BioReq) request);
+                break;
+            case FOLLOW:
+                follow((FollowReq) request);
+                break;
+            case UNFOLLOW:
+                unfollow((UnFollowReq) request);
+                break;
+            case SHOW_TIMELINE:
+                show_timeline((TimelineReq) request);
+                break;
+            case BLOCK:
+                blockUser((BlockReq) request);
+                break;
+            case UNBLOCK:
+                unblock((UnblockReq) request);
+                break;
+            case GET_FOLLOWERS:
+                get_followers((FollowersListReq) request);
+                break;
+            case GET_FOLLOWING:
+                get_following((FollowingListReq) request);
+                break;
+            case SEARCH:
+                search((SearchReq) request);
+                break;
+            case TWEET_INFO:
+                tweet_info((TweetInfoReq) request);
+                break;
+            case USER_PROFILE:
+                get_userProfile((UserProfileReq) request);
+                break;
+            default:
+                throw new InvalidTypeException();
+        }
+    }
+
+
+    private void get_userProfile(UserProfileReq request){
+        sendResponse(service.get_userProfile(request));
+    }
+
+    private void tweet_info(TweetInfoReq request){
+        sendResponse(service.get_Tweet_Info(request));
+    }
+
+    private void search(SearchReq request){
+        if (request.getSearchText().startsWith("#")){
+            sendResponse(service.search_with_hashtag(request));
+        }
+        else {
+            sendResponse(service.search(request));
+        }
+    }
+
+    private void get_followers(FollowersListReq request){
+        sendResponse(service.getFollowersList(request));
+    }
+
+    private void get_following(FollowingListReq request){
+        sendResponse(service.getFollowingList(request));
+    }
+
+    private void get_profile(MyProfileReq request){
+        sendResponse(service.get_profile(request));
+    }
+
+    private void blockUser(BlockReq request)
+    {
+        handleResponse(service.block_user(request) , request);
+    }
+
+    private void set_avatar(AvatarReq request)
+    {
+        handleResponse(service.set_avatar(request) , request);
+    }
+
+    private void set_header(HeaderReq request)
+    {
+        handleResponse(service.set_header(request) , request);
+    }
+
+    private void set_bio(BioReq request)
+    {
+        handleResponse(service.set_bio(request) , request);
+    }
+
+    private void follow(FollowReq request)
+    {
+        handleResponse(service.follow(request) , request);
+    }
+
+    private void unfollow(UnFollowReq request)
+    {
+        handleResponse(service.unfollow(request) , request);
+    }
+
+    private void show_timeline(TimelineReq request)
+    {
+        sendResponse(service.show_timeline(request));
+    }
+
+    private void unblock(UnblockReq request)
+    {
+        handleResponse(service.unblock(request) , request);
+    }
+
+    private void handleResponse(Response response , UserRequest request)
+    {
+        sendResponse(response);
+    }
+
+    private void sendResponse(Response response)
+    {
+        try {
+            sender.sendResponse(response);
+        } catch (UserThreadNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+}
